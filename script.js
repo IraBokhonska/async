e7b52d909315b2a49b3bc0bef22f822e4d24a806
Ira Bokhonska <bokhonska22241@gmail.com>
2023-06-07 14:33:27 +0300

    added search

--- script.js
+++ script.js
@@ -7,30 +7,51 @@ const getProductsByAxios = async () => {
 
 getProductsByAxios(); */ //виведено масив об'єктів продуктів API
 
-const BASE_URL = "http://www.omdbapi.com/?apikey=c30fe8d5&s=min&type=movie";
+const filmKey = "c30fe8d5";
+const userSearchInput = document.querySelector(".search");
+const userSearchType = document.querySelectorAll(".type");
+
+const BASE_URL = `http://www.omdbapi.com/?apikey=${filmKey}`;
 
 const listElement = document.querySelector(".film__list");
 
 const addFilms = (item) => {
   const newListElement = document.createElement("li");
 
-  // newListElement.classList = "film__item"; //можна додати клас і офомити його в css
-
   newListElement.innerHTML = `
-  <h2 class="h2">${item.Title}</h2>
-  <p>${item.Year}</p>
-  <span>${item.imdbID}</span>
-  <span>${item.Type}</span>
-  <img src="${item.Poster}" alt=""/>
+    <h2 class="h2">${item.Title}</h2>
+    <p>${item.Year}</p>
+    <span>${item.imdbID}</span>
+    <span>${item.Type}</span>
+    <img src="${item.Poster}" alt=""/>
   `;
 
   listElement.appendChild(newListElement);
 };
 
 const getFilmsByAxios = async () => {
-  const resp = await axios.get(`${BASE_URL}`);
+  const filmSearch = userSearchInput.value;
+
+  let filmType = "";
+
+  userSearchType.forEach((radio) => {
+    if (radio.checked) {
+      filmType = radio.value;
+    }
+  });
+
+  const url = `${BASE_URL}&s=${filmSearch}&type=${filmType}`;
+
+  const resp = await axios.get(url);
+
+  listElement.innerHTML = "";
 
   resp.data.Search.forEach(addFilms);
 };
 
-getFilmsByAxios();
+const form = document.querySelector(".form__container");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getFilmsByAxios();
+});
